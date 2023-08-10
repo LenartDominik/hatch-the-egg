2055902d3fe595644da38ba997a9ae34fddd6f78
Dominik Lenart <dominik.lenart84@gmail.com>
2023-08-10 12:33:52 +0200

    egg-hatching

--- src/modules/egg.ts
+++ src/modules/egg.ts
@@ -1,15 +1,36 @@
 export enum EggState {
-	Egg = 'egg',
-	Tamagtochi = 'tamagotchi',
+  Egg = "egg",
+  Tamagtochi = "tamagotchi",
 }
-export class Egg {
-	eggClicks: number = 0;
-	assets = new Map([
-		[EggState.Egg, 'assets/egg.svg'],
-		[EggState.Tamagtochi, 'assets/tamagotchi.svg'],
-	]);
 
-	tapEgg() {
-		this.eggClicks++;
-	}
+interface EggParams {
+  clicksToHatch: number;
+  onEggHatch: () => void;
+}
+
+export class Egg {
+  eggClicks: number = 0;
+  clicksToHatch: number;
+  assets = new Map([
+    [EggState.Egg, "assets/egg.svg"],
+    [EggState.Tamagtochi, "assets/tamagotchi.svg"],
+  ]);
+  onEggHatch: () => void;
+
+  constructor({ clicksToHatch, onEggHatch }: EggParams) {
+    this.clicksToHatch = clicksToHatch;
+    this.onEggHatch = onEggHatch;
+  }
+
+  tapEgg() {
+    if (this.eggClicks >= this.clicksToHatch) {
+      return;
+    }
+
+    this.eggClicks++;
+
+    if (this.eggClicks >= this.clicksToHatch) {
+      this.onEggHatch();
+    }
+  }
 }
--- src/modules/game.ts
+++ src/modules/game.ts
@@ -2,14 +2,21 @@ import { Egg, EggState } from "./egg.js";
 interface GameParams {
   eggElement: HTMLImageElement | null;
   counterElement: HTMLParagraphElement | null;
+  resultElement: HTMLParagraphElement | null;
 }
+
 interface IGame extends GameParams {}
+
 export class Game implements IGame {
 	counterElement: HTMLParagraphElement | null = null;
+	resultElement: HTMLParagraphElement | null = null;
 	eggElement: HTMLImageElement | null = null;
 	stopWatch: number | null = null;
 	secondsPassed: number = 0;
-	eggInstance: Egg = new Egg();
+	eggInstance: Egg = new Egg({
+		clicksToHatch: 30,
+		onEggHatch: this.hatchEgg.bind(this),
+	});
 
 	init(params: GameParams) {
 		if (!params.counterElement || !params.eggElement) {
@@ -17,12 +24,11 @@ export class Game implements IGame {
 		}
 		this.counterElement = params.counterElement;
 		this.eggElement = params.eggElement;
+		this.resultElement = params.resultElement;
 		this.displayEggClicks();
 		this.mountEgg();
-		console.log('this', this);
 		console.log('Game started');
 	}
-
 	displayEggClicks() {
 		if (!this.counterElement) {
 			throw new Error('Counter element not found');
@@ -32,21 +38,27 @@ export class Game implements IGame {
 
 	startStopWatch() {
 		this.stopWatch = setInterval(() => {
-			this.secondsPassed++;
-		}, 1000);
+			this.secondsPassed = this.secondsPassed + 100;
+		}, 100);
+	}
+
+	stopStopWatch() {
+		if (!this.stopWatch) {
+			throw new Error('Stop watch not found');
+		}
+
+		clearInterval(this.stopWatch);
 	}
 
 	updateEggClick() {
 		this.eggInstance.tapEgg();
 		this.displayEggClicks();
-
 		switch (this.eggInstance.eggClicks) {
 			case 1:
 				this.startStopWatch();
 				break;
 		}
 	}
-
 	mountEgg() {
 		if (!this.eggElement) {
 			throw new Error('Egg element not found');
@@ -55,8 +67,31 @@ export class Game implements IGame {
 		if (!eggImageSrc) {
 			throw new Error('Egg image src not found');
 		}
+
 		this.eggElement.src = eggImageSrc;
 		this.eggElement.addEventListener('click', this.updateEggClick.bind(this));
+	}
+
+	hatchEgg() {
+		if (!this.eggElement) {
+			throw new Error('Egg element not found');
+		}
+
+		const eggImageSrc = this.eggInstance.assets.get(EggState.Tamagtochi);
+
+		if (!eggImageSrc) {
+			throw new Error('Egg image src not found');
+		}
+
+		if (!this.resultElement) {
+			throw new Error('Result element not found');
+		}
+
+		this.eggElement.src = eggImageSrc;
+		this.resultElement.innerText =
+			(this.secondsPassed / 1000).toString() + ' seconds';
+
+		this.stopStopWatch();
 	}
 }
     
